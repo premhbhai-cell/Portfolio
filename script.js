964635c80f972820daee62a1f5b637cbbf5e0a76
Prem kumar <premhbhai@gmail.com>
2026-03-12 15:17:00 +0530

    html,css,js,imgs added

---
+++ script.js
@@ -0,0 +1,10 @@
+
+// hedder toggle buttom
+const menuBtn = document.getElementById("menu-btn")
+const nav = document.getElementById("nav")
+
+menuBtn.onclick = () => {
+
+nav.classList.toggle("active")
+
+}
